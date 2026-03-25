b16b041421496f7b6f4f94be4195609f9300d238
sxsxsx-git <hairdo.zingier.7z@icloud.com>
2026-03-26 01:13:43 +0900

    Bump app version to 0.1.1

--- scripts/build-app-bundle.sh
+++ scripts/build-app-bundle.sh
@@ -94,9 +94,9 @@ cat > "$CONTENTS_DIR/Info.plist" <<'PLIST'
   <key>CFBundleIconFile</key>
   <string>AppIcon</string>
   <key>CFBundleShortVersionString</key>
-  <string>0.1.0</string>
+  <string>0.1.1</string>
   <key>CFBundleVersion</key>
-  <string>1</string>
+  <string>2</string>
   <key>LSMinimumSystemVersion</key>
   <string>14.0</string>
   <key>NSHighResolutionCapable</key>
